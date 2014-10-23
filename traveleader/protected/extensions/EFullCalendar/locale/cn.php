0425524168a17ecc19cb33da60def738b5d639b5
BinLoveMiao <longbin.lai@gmail.com>
2014-10-23 11:43:36 +1100

    Commit the EFullCalendar extension

---
+++ traveleader/protected/extensions/EFullCalendar/locale/cn.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    'monthNames'=>array(
+        '一月','二月','三月','四月','五月','六月',
+        '七月','八月','九月','十月','十一月','十二月'
+    ),
+    'monthNamesShort'=>array(
+        'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'
+    ),
+    'firstDay'=>1,
+    'dayNames'=>array(
+        '周日','周一','周二','周三','周四','周五','周六'
+    ),
+    'dayNamesShort'=>array(
+        '日','一','二','三','四','五','六'
+    ),
+    'buttonText'=>array(
+        'today'=>'今天',
+        'month'=>'月',
+        'week'=>'周',
+        'day'=>'天'
+    ),
+);
